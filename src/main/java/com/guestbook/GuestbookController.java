72f106e78c5f8b9a8b6b7ff89c01abef940dc4dc
maanick <maanickavasagam@gmail.com>
2021-04-05 18:00:54 -0600

    Red Test for fetch empty comments with controller

--- src/main/java/com/guestbook/GuestbookController.java
+++ src/main/java/com/guestbook/GuestbookController.java
@@ -1,10 +1,23 @@
 package com.guestbook;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+
 public class GuestbookController {
+
+    public GuestbookService guestbookService;
+
+    @GetMapping("comments")
+    public List<String> fetchComments(){
+        return guestbookService.fetchEntries();
+    }
+
 
 
 
--- src/test/java/com/guestbook/GuestbookControllerIT.java
+++ src/test/java/com/guestbook/GuestbookControllerIT.java
@@ -1,4 +1,31 @@
 package com.guestbook;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class GuestbookControllerIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void fetchEmptyList() throws Exception {
+
+        mockMvc.perform(get("/comments"))
+                .andExpect(status().isOk());
+
+    }
+
 }
