eae7037502e819379a03c67dee56aaedeff952f6
maanick <maanickavasagam@gmail.com>
2021-04-06 16:19:47 -0600

    Green Test to return empty list of comments

--- src/main/java/com/guestbook/GuestbookController.java
+++ src/main/java/com/guestbook/GuestbookController.java
@@ -1,6 +1,7 @@
 package com.guestbook;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-
 public class GuestbookController {
-
+    @Autowired
     public GuestbookService guestbookService;
 
     @GetMapping("comments")
